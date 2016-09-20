5fdc2100e4f6f181992cf8d0a6073f93baad5dc1
TyVik <tyvik8@gmail.com>
2016-09-20 21:39:56 +0300

    Implement model for parsed data.

--- habr/items.py
+++ habr/items.py
@@ -9,6 +9,6 @@ import scrapy
 
 
 class HabrItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    author = scrapy.Field()
+    stars = scrapy.Field()
--- habr/spiders/habr.py
+++ habr/spiders/habr.py
@@ -1,4 +1,7 @@
 from scrapy.spiders import CrawlSpider
+from scrapy.selector import Selector
+
+from habr.items import HabrItem
 
 
 class HabrSpider(CrawlSpider):
@@ -8,6 +11,13 @@ class HabrSpider(CrawlSpider):
         "http://habrahabr.ru"  # откуда начинать
     ]
 
-    def parse(self, response):  # метод обработки ответа
-        with open('response.html', 'wb') as f:
-            f.write(response.body)
+    def parse(self, response):
+        root = Selector(response)
+        # да, классы необходимо указывать полностью
+        posts = root.xpath('//div[@class="post post_teaser shortcuts_item"]')
+        for post in posts:
+            item = HabrItem()
+            item['title'] = post.xpath('.//a[@class="post__title_link"]/text()').extract()[0]
+            item['author'] = post.xpath('.//a[@class="post-author__link"]/text()')[1].extract().rstrip()
+            item['stars'] = post.xpath('.//span[@class="favorite-wjt__counter js-favs_count"]/text()').extract()[0]
+            yield item
