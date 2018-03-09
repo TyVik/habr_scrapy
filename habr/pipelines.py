9b03da798a143ab6cb7058d06a2cef1adfdc8348
TyVik <tyvik8@gmail.com>
2018-03-09 19:38:43 +0300

    Remove dump to json from pipeline.

--- habr/pipelines.py
+++ habr/pipelines.py
@@ -1,20 +1,5 @@
-import json
-
-
 class HabrPipeline(object):
-    def __init__(self):
-        self.file = open('habr.json', 'wb')
-
-    def write(self, text):
-        self.file.write(bytes(text, 'UTF-8'))
-
-    def open_spider(self, spider):
-        self.write("[\n")
-
-    def close_spider(self, spider):
-        self.write("]")
-
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + ",\n"
-        self.write(line)
+        item['title'] = item['title'].upper()
+        item['author'] = 'Ув, {}'.format(item['author'])
         return item
